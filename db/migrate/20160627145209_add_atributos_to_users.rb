0daf84537c2c7edec286c5312a68338c7e3dfcdf
josheramirez <joseinformatico2015@gmail.com>
2016-07-03 15:11:51 -0400

    password y produccion nuevo

--- db/migrate/20160627145209_add_atributos_to_users.rb
+++ db/migrate/20160627145209_add_atributos_to_users.rb
@@ -1,6 +1,6 @@
 class AddAtributosToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :numero_contacto, :sring
+    add_column :users, :numero_contacto, :string
     add_column :users, :calle_envio, :string
     add_column :users, :numero_envio, :string
     add_column :users, :comuna_envio, :string
